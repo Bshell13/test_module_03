ba692d54ab2517341ce49a7c8073daab71cae9e8
Brandon Shellenberger <brandonshellenberger93@gmail.com>
2024-01-21 09:01:55 -0600

    Final Change

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-print("hello world")
-print("hello again")
+def unique_sorting(list_unsorted) -> list:
+    '''
+    finds the unique elements in a list and sorts them
+    : param list_unsorted: a list of unsorted elements
+    '''
+    list_sorted = list_unsorted.unique()
+    return list_sorted
+
+list_unsorted = [1,4,5,7,2,4,90,7,8,4,5,3]
+print(unique_sorting(list_unsorted))
